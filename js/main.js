627d5cdd56dc2b39c1aa1001dae9603dde4d9992
DESKTOP-TP0PGUH\jeffe <jeffev123@gmail.com>
2024-07-11 16:27:30 -0300

    adicionado idioma ingles

--- js/main.js
+++ js/main.js
@@ -147,3 +147,12 @@
 
 
 }());
+
+let lang = navigator.language.slice(0, 2); // Get the first two letters of user's language preference
+
+// Hide elements that don't correspond to the user's preferred language
+if (lang === 'en') {
+  document.querySelectorAll('.second-language').forEach(elem => elem.style.display = 'none');
+} else {
+  document.querySelectorAll('.english').forEach(elem => elem.style.display = 'none');
+}
